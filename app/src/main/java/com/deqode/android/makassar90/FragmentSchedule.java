56f51d02a5fea1747219a9df5020fabc45a1b3bc
Nur Indah <indah.pgnt@gmail.com>
2017-02-08 21:24:34 +0700

    Revisi 2, desain selese

---
+++ app/src/main/java/com/deqode/android/makassar90/FragmentSchedule.java
@@ -0,0 +1,104 @@
+package com.deqode.android.makassar90;
+
+
+import android.os.Bundle;
+import android.support.annotation.Nullable;
+import android.support.design.widget.TabLayout;
+import android.support.v4.app.Fragment;
+import android.support.v4.app.FragmentManager;
+import android.support.v4.app.FragmentPagerAdapter;
+import android.support.v4.view.ViewPager;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+
+/**
+ * A simple {@link Fragment} subclass.
+ */
+public class FragmentSchedule extends Fragment {
+
+    public static TabLayout tabLayout;
+    public static ViewPager viewPager;
+    public static int int_items = 2 ;
+    public FragmentSchedule() {
+        // Required empty public constructor
+    }
+
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        // Inflate the layout for this fragment
+        View x = inflater.inflate(R.layout.fragment_fragment_schedule, container, false);
+        tabLayout = (TabLayout) x.findViewById(R.id.tabs);
+        viewPager = (ViewPager) x.findViewById(R.id.viewpager);
+
+        viewPager.setAdapter(new MyAdapter(getChildFragmentManager()));
+
+        /**
+         * Now , this is a workaround ,
+         * The setupWithViewPager dose't works without the runnable .
+         * Maybe a Support Library Bug .
+         */
+
+        tabLayout.post(new Runnable() {
+            @Override
+            public void run() {
+                tabLayout.setupWithViewPager(viewPager);
+            }
+        });
+
+        return x;
+
+
+    }
+
+    class MyAdapter extends FragmentPagerAdapter {
+
+        public MyAdapter(FragmentManager fm) {
+            super(fm);
+        }
+
+        /**
+         * Return fragment with respect to Position .
+         */
+
+        @Override
+        public Fragment getItem(int position)
+        {
+            switch (position){
+                case 0 : return new FragmentSabtu();
+                case 1 : return new FragmentMinggu();
+
+            }
+            return null;
+        }
+
+        @Override
+        public int getCount() {
+
+            return int_items;
+
+        }
+
+        /**
+         * This method returns the title of the tab according to the position.
+         */
+
+        @Override
+        public CharSequence getPageTitle(int position) {
+
+            switch (position){
+                case 0 :
+                    return "SABTU"+"APRIL12";
+                case 1 :
+                    return "MINGGU"+"APRIL13";
+            }
+            return null;
+        }
+    }
+
+}
+
+
